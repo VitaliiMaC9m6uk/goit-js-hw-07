c70323a8985f6eb4addbf25872ed7592cc4924e0
VitaliiMaC9m6uk <mac9m6uk@gmail.com>
2023-02-28 16:07:22 +0100

    зробив 1 завдання

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,57 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
 
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+gallery.insertAdjacentHTML('afterbegin', createGalleryItems(galleryItems));
+gallery.addEventListener('click', clickOnGallery)
+
+function createGalleryItems(image) {    
+
+    return image.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </div>`
+    })
+        .join('');   
+    
+}
+
+function clickOnGallery(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const closeModal = event => {
+        if (event.code === 'Escape') {
+            instance.close();
+        }
+    };
+
+    const instance = basicLightbox.create(`
+	 	<img width="1280" src="${event.target.dataset.source}"> `,
+        {
+            onShow: instance => {
+                window.addEventListener('keydown', closeModal)
+            },
+            onClose: instance => {
+                window.removeEventListener('keydown', closeModal)
+            },
+        }
+        
+       
+    );
+    instance.show();
+}
+    
+
